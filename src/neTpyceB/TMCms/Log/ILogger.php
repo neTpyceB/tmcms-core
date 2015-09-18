396e4ea864e2a1a92258ed7c99aac3919b5d6a5d
neTpyceB <pvman@inbox.ru>
2015-09-19 00:35:59 +0300

    ILogger interface

---
+++ src/neTpyceB/TMCms/Log/ILogger.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace neTpyceB\TMCms\Log;
+
+/**
+ * Interface ILogger
+ */
+interface ILogger {
+
+    const WRITE_LOG = '*';
+
+    const WRITE_ERR = 'E';
+
+    /**
+     * @return mixed
+     */
+    public function startLog();
+
+    /**
+     * @param string $str
+     */
+    public function log($str);
+
+    /**
+     * @param string $str
+     */
+    public function err($str);
+
+    /**
+     * @param string $str
+     * @param string $flag
+     */
+    public function write($str = '', $flag = ILogger::WRITE_LOG);
+
+    /**
+     * @return mixed
+     */
+    public function endLog();
+}
