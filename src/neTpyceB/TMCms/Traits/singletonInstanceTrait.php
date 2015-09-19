7b3997d1bd780959948a2a8cd95cb2f20e6f6aa9
Vadims Petrusevs <pvman@inbox.ru>
2015-09-19 15:24:37 +0300

    singleton Traits created

---
+++ src/neTpyceB/TMCms/Traits/singletonInstanceTrait.php
@@ -0,0 +1,23 @@
+<?php
+namespace neTpyceB\TMCms\Traits;
+
+defined ('INC') or exit;
+
+/**
+ * Trait singletonInstanceTrait means that class MAY be used as singleton to get created instance.
+ * May be used in any required class
+ * @package neTpyceB\TMCms\Traits
+ */
+trait singletonInstanceTrait {
+    /**
+     * @var $this
+     */
+    private static $instance;
+
+    /**
+     * @return $this
+     */
+    public static function getInstance() {
+        return self::$instance ? self::$instance : (self::$instance = new self);
+    }
+}
