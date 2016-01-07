0ae4c28e4d4417f72a9833b0cf1d8a43cf95c704
neTpyceB <pvman@inbox.ru>
2016-01-07 13:50:02 +0200

    DB structure for landing links

--- src/Routing/Entity/PagesQuicklinkEntityRepository.php
+++ src/Routing/Entity/PagesQuicklinkEntityRepository.php
@@ -6,6 +6,26 @@ use TMCms\Orm\EntityRepository;
 
 class PagesQuicklinkEntityRepository extends EntityRepository
 {
+    protected $table_structure = [
+        'fields' => [
+            'page_id' => [
+                'type' => 'int',
+                'unsigned' => true,
+            ],
+            'href' => [
+                'type' => 'varchar',
+            ],
+            'searchword' => [
+                'type' => 'bool',
+            ],
+        ],
+        'indexes' => [
+            'page_id' => [
+                'type' => 'key',
+            ],
+        ],
+    ];
+
     public function setWhereAnyName($value)
     {
         $this->addWhereFieldAsString(' OR `name` = "' . $value . '" ');
