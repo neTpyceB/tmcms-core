eef30a542d34e92f1e8d2bc6241e369544558c4c
beta <beta@devp.lv>
2016-03-18 17:00:07 +0200

    method $data->getIds() update(revert)

--- src/Orm/EntityRepository.php
+++ src/Orm/EntityRepository.php
@@ -379,7 +379,7 @@ class EntityRepository {
         $pairs = [];
         foreach ($this->getAsArrayOfObjects() as $v) {
             /** @var Entity $v */
-            //$v->loadDataFromDB(); CACHE
+            $v->loadDataFromDB();
 
             $key_method = 'get' . ucfirst($key_field);
             $value_method = 'get' . ucfirst($value_field);
