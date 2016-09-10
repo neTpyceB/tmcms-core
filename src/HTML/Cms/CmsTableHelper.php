68cbd6b423a34d791a625a97905be2e1c042f71e
neTpyceB <pvman@inbox.ru>
2016-09-10 19:15:32 +0300

    Table render helper updates with filters and more options

--- src/HTML/Cms/CmsTableHelper.php
+++ src/HTML/Cms/CmsTableHelper.php
@@ -6,6 +6,8 @@ use TMCms\HTML\Cms\Column\ColumnActive;
 use TMCms\HTML\Cms\Column\ColumnData;
 use TMCms\HTML\Cms\Column\ColumnDelete;
 use TMCms\HTML\Cms\Column\ColumnEdit;
+use TMCms\HTML\Cms\Filter\Select;
+use TMCms\HTML\Cms\Filter\Text;
 
 class CmsTableHelper {
     public static function outputTable(array $params) {
@@ -70,9 +72,71 @@ class CmsTableHelper {
                     break;
             }
 
+            // Title
+            if (isset($column['title'])) {
+                $column->setTitle($column['title']);
+            }
+
+            // Link
+            if (isset($column['href'])) {
+                $column->setHref($column['href']);
+            }
+
+            // Paired array
+            if (isset($column['pairs'])) {
+                $column->setPairedDataOptionsForKeys($column['pairs']);
+            }
+
             if ($column) {
                 $table->addColumn($column);
             }
+        }
+
+        // Apply filter
+        if (isset($params['filters']) || $params['caption']) {
+            $filter_form = new FilterForm;
+
+            // Top caption above table
+            if (isset($params['caption'])) {
+                $filter_form->setCaption($params['caption']);
+            }
+
+            // Render filters
+            if (isset($params['filters'])) {
+                foreach ($params['filters'] as $filter_key => $filter_data) {
+                    // Title is obligate
+                    if (!isset($filter_data['title'])) {
+                        $filter_data['title'] = $filter_key;
+                    }
+
+                    //Filter types
+                    $filter = NULL;
+                    switch ($filter_data['type']) {
+                        case 'text':
+                            $filter = Text::getInstance($filter_key);
+                            break;
+                        case 'select':
+                            $filter = Select::getInstance($filter_key);
+                            break;
+                    }
+
+                    // Options for selects
+                    if (isset($filter_data['options'])) {
+                        $filter->setOptions($filter_data['options']);
+                    }
+
+                    // Ignore values in selects
+                    if (isset($filter_data['ignore'])) {
+                        $filter->ignoreValue($filter_data['ignore']);
+                    }
+
+                    if ($filter) {
+                        $filter_form->addFilter($filter_data['title'], $filter);
+                    }
+                }
+            }
+
+            $table->attachFilterForm($filter_form);
         }
 
         return $table;
