cf9510c59199c1158d65f39caf1021d6e89ad42e
neTpyceB <pvman@inbox.ru>
2016-12-28 14:10:12 +0200

    Check title in menu

--- src/Admin/Menu.php
+++ src/Admin/Menu.php
@@ -106,7 +106,11 @@ class Menu
                             <li class="heading">
                                 <h3 class="uppercase"><?= $v ?></h3>
                             </li>
-                        <?php else: ?>
+                        <?php else:
+                            if (!isset($v['title'])) {
+                                $v['title'] = $k;
+                            }
+                            ?>
                             <li class="<?= P == $k ? 'active open' : '' ?>">
                                 <a href="#">
                                     <i class="icon-<?= isset($v['icon']) ? $v['icon'] : 'home' ?>"></i>
@@ -115,7 +119,11 @@ class Menu
                                 </a>
                                 <?php if (isset($this->_menu[$k]['items']) && is_array($this->_menu[$k]['items'])): ?>
                                     <ul class="sub-menu">
-                                        <?php foreach ($this->_menu[$k]['items'] as $k_in => $v_in): ?>
+                                        <?php foreach ($this->_menu[$k]['items'] as $k_in => $v_in):
+                                            if (!isset($v_in['title'])) {
+                                                $v_in['title'] = $k_in;
+                                            }
+                                            ?>
                                         <li class="<?= (P == $k && P_DO == $k_in) ? 'active' : '' ?>">
                                             <a href="?p=<?= $k . '&do=' . $k_in ?>">
                                                 <i class="icon-<?= isset($v_in['icon']) ? $v_in['icon'] : 'home' ?>"></i>
