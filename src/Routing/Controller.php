930636a049455600d1f761d1c22572a1eb034c0b
neTpyceB <pvman@inbox.ru>
2017-02-01 19:14:32 +0200

    MVC classes for Views and Controllers, also for editable Components

---
+++ src/Routing/Controller.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace TMCms\Routing;
+
+use TMCms\Templates\Components;
+
+defined('INC') or exit;
+
+/**
+ * Class CmsController
+ */
+class Controller
+{
+    /**
+     * @var array
+     */
+    public static $components = array();
+    /**
+     * @var MVC
+     */
+    protected $mvc_instance;
+
+    /**
+     * @param MVC $mvc
+     */
+    public function __construct(MVC $mvc)
+    {
+        $this->mvc_instance = $mvc;
+    }
+
+    /**
+     * Overwrite this method in required Controller class
+     * @return array
+     */
+    public static function getComponents()
+    {
+        return [];
+        /* EXAMPLE
+        return [
+            'title' = [], // Means default type text
+            'text' => [
+                'type' => 'textarea', // textarea | text
+    			'edit' => 'files' // wysiwyg | pages | files
+            ]
+        ];
+        */
+    }
+
+    /**
+     * @param string $name
+     * @return string
+     */
+    public function getComponentValue($name)
+    {
+        return Components::get($name, $this->getMvc()->getComponentName());
+    }
+
+    /**
+     *
+     * @return MVC
+     */
+    protected function getMvc()
+    {
+        return $this->mvc_instance;
+    }
+
+    /**
+     * This function is called first after Controller creation - use it to load shared data
+     */
+    public function setUp() {
+
+    }
+
+    /**
+     * Check that modifier in charge is set in template
+     * @param string $modifier
+     * @return bool
+     */
+    public function checkModifier($modifier) {
+        return in_array($modifier, $this->getMvc()->getModifiers());
+    }
+}
---
+++ src/Templates/Components.php
@@ -0,0 +1,308 @@
+<?php
+
+namespace TMCms\Templates;
+
+use TMCms\Admin\Structure\Entity\PageComponent;
+use TMCms\Admin\Structure\Entity\PageComponentRepository;
+use TMCms\Admin\Structure\Entity\PageComponentHistory;
+use TMCms\Admin\Structure\Entity\PageComponentHistoryRepository;
+use TMCms\Admin\Structure\Entity\PageComponentCustomEntityRepository;
+use TMCms\Cache\Cacher;
+use TMCms\Config\Settings;
+use TMCms\Routing\Controller;
+
+defined('INC') or exit;
+
+/**
+ * Class Components
+ */
+class Components
+{
+    /**
+     * @var string regular expression to find all components that are like {%file_and_class:method|possible_modifier_one|possible_modifier_two%}
+     */
+    public static $preg_pattern = '/{%([a-zA-Z0-9-_]+):?([a-zA-Z0-9-_]*):?([a-zA-Z0-9-_]*)\|?([a-zA-Z0-9-_\|]*)\|?([a-zA-Z0-9-_\|]*)%}/';
+    public static $preg_pattern_alternative = '/\[%([a-zA-Z0-9-_]+):?([a-zA-Z0-9-_]*):?([a-zA-Z0-9-_]*)\|?([a-zA-Z0-9-_\|]*)%\]/'; // Used in TWIG templates
+    /**
+     * @var array
+     */
+    private static $_init = [];
+    /**
+     * @var array
+     */
+    private static $_components = [];
+    /**
+     * @var bool pattern for finding components, can use ald versions saved in history
+     */
+    private static $_version = false;
+
+    /**
+     * @param string $file template file path
+     * @return array
+     */
+    public static function outputForCms($file)
+    {
+        $html = file_get_contents($file);
+        $parsed_components = self::parseForComponents($html);
+
+        $components_classes_array = $parsed_components[1];
+        $components_methods_array = $parsed_components[2];
+
+        $components_array = [];
+
+        foreach ($components_classes_array as $index => $class) {
+            $components_array[$class][] = $components_methods_array[$index] ? $components_methods_array[$index] : 'index';
+        }
+
+        $elements = [];
+
+        foreach ($components_array as $component_class => $component_methods) {
+            if ($component_class == 'plugin') {
+                $elements[$component_class . '_' . $component_class] = [
+                    'type' => 'plugin',
+                    'file' => $component_class,
+                    'class' => $component_class,
+                    'elements' => [
+                        'select_plugin' => [
+                            'type' => 'select',
+                            'options' => ['' => ''] + Plugin::getInstance()->getPluginFilePairs()
+                        ]
+                    ]
+                ];
+            } else {
+                require_once DIR_FRONT_CONTROLLERS . $component_class . '.php';
+
+                $controller_class = ucfirst($component_class) . 'Controller';
+
+                if (!class_exists($controller_class)) {
+                    $controller_class = str_replace('_', '', $controller_class);
+                }
+
+                $elements_data = $controller_class::getComponents();
+
+                $class_method = get_class_methods($controller_class);
+
+                foreach ($class_method as $component_method) {
+                    $controller_method = 'getComponents_' . $component_method;
+
+                    if (method_exists($controller_class, $controller_method)) {
+                        $elements_data = array_merge($elements_data, $controller_class::$controller_method());
+                    }
+
+                    $elements[$component_class] = [
+                        'type' => 'component',
+                        'file' => $component_class,
+                        'class' => $component_class,
+                        'elements' => $elements_data
+                    ];
+                }
+            }
+        }
+
+        return $elements;
+    }
+
+    /**
+     * @param string $html
+     * @return array
+     */
+    public static function parseForComponents($html)
+    {
+        $res = [];
+        preg_match_all(self::$preg_pattern_alternative, $html, $res);
+        if (!isset($res[0]) || !$res[0]) {
+            preg_match_all(self::$preg_pattern, $html, $res);
+        }
+        return $res;
+    }
+
+    /**
+     * @param string $component
+     * @param string $class
+     * @return string|null
+     */
+    public static function get($component, $class)
+    {
+        // Get all data for component class in one query
+        if (!isset(self::$_init[$class])) {
+            self::init($class);
+        }
+
+        $component = $class . '_' . $component;
+
+        // Return selected component
+        if (isset(self::$_components[$class][$component])) {
+            return self::$_components[$class][$component];
+        }
+
+        return NULL;
+    }
+
+    /**
+     * Load all components for class in private cache variable
+     * @param string $class
+     */
+    private static function init($class)
+    {
+        self::$_init[$class] = true;
+
+        if (isset($_GET['cms_content_version']) && ctype_digit($_GET['cms_content_version'])) {
+            self::$_version = (int)$_GET['cms_content_version'];
+        }
+
+        $cache_key = 'components_' . PAGE_ID . '_v_' . self::$_version;
+        $res = NULL;
+        if (Settings::isCacheEnabled()) {
+            $res = Cacher::getInstance()->getDefaultCacher()->get($cache_key);
+        }
+
+        if (is_array($res)) { // Set from cache
+            self::$_components[$class] = $res;
+        } else { // Get from DB
+            if (self::$_version) {
+                // From history
+                $components_collection = new PageComponentHistoryRepository();
+                $components_collection->setWhereVersion(self::$_version);
+            } else {
+                // From current version
+                $components_collection = new PageComponentRepository();
+            }
+            $components_collection->setWherePageId(PAGE_ID);
+            self::$_components[$class] = $components_collection->getPairs('data', 'component');
+
+            // Get Custom components
+            $customs = new PageComponentCustomEntityRepository();
+            $customs->setWherePageId(PAGE_ID);
+            $customs->setWhereComponent($class);
+            $customs->addOrderByField('order');
+
+            $custom_components_in_database = $customs->getAsArrayOfObjectData(true);
+            foreach ($custom_components_in_database as $custom) {
+                self::$_components[$class][$class . '_' . $custom['tab']][$custom['order']][$custom['name']] = $custom['value'];
+            }
+
+            // Make all custom components have all fields
+            $custom_components_in_controller = self::getControllerCustomComponents($class);
+            foreach ($custom_components_in_database as $custom) {
+                //
+                foreach (self::$_components[$class][$class . '_' . $custom['tab']] as $order => $data) {
+                    if (count(self::$_components[$class][$class . '_' . $custom['tab']][$order]) != count($custom_components_in_controller[$class . '_' . $custom['tab']])) {
+                        foreach ($custom_components_in_controller[$class . '_' . $custom['tab']] as $field_key => $field_value) {
+                            if (!isset(self::$_components[$class][$class . '_' . $custom['tab']][$order][$field_key])) {
+                                self::$_components[$class][$class . '_' . $custom['tab']][$order][$field_key] = ''; // Set empty field
+                            }
+                        }
+                    }
+                }
+
+            }
+
+            // Save to cache
+            if (Settings::isCacheEnabled()) {
+                Cacher::getInstance()->getDefaultCacher()->set($cache_key, self::$_components[$class]);
+            }
+        }
+    }
+
+    /**
+     * Get one component data by page id and name
+     * @param $component
+     * @param $page_id
+     * @return string
+     */
+    public static function getComponentByPageId($component, $page_id)
+    {
+        $page_id = (int)$page_id;
+        $component = sql_prepare($component);
+
+        $cache_key = 'components_' . $page_id . '_c_' . $component;
+        $res = NULL;
+
+        if (Settings::isCacheEnabled()) {
+            $res = Cacher::getInstance()->getDefaultCacher()->get($cache_key);
+
+            // Set from cache
+            if ($res) {
+                return $res;
+            }
+        }
+
+        // Get from DB
+        $res = NULL;
+        /** @var PageComponentHistory $page */
+        $page = PageComponentRepository::findOneEntityByCriteria([
+            'page_id' => $page_id,
+            'component' => $component
+        ]);
+
+        if ($page) {
+            $res = $page->getData();
+        }
+
+        // Save to cache
+        if (Settings::isCacheEnabled()) {
+            Cacher::getInstance()->getDefaultCacher()->set($cache_key, $res);
+        }
+
+        return $res;
+    }
+    public static function getComponentsByPageId($page_id)
+    {
+        $page_id = (int)$page_id;
+
+        $cache_key = 'components_' . $page_id;
+
+        if (Settings::isCacheEnabled()) {
+            $res = Cacher::getInstance()->getDefaultCacher()->get($cache_key);
+
+            // Set from cache
+            if ($res) {
+                return $res;
+            }
+        }
+
+        // Get from DB
+        $res = [];
+        /** @var PageComponentHistory $page */
+        $page = PageComponentRepository::findAllEntitiesByCriteria([
+            'page_id' => $page_id
+        ]);
+
+        $page_arr = [];
+
+        if ($page) {
+
+            foreach($page as $k => $v):
+                /** @var PageComponent $v */
+                $page_arr[$v->getComponent()] = $v->getData();
+            endforeach;
+
+            $res = $page_arr;
+        }
+
+        // Save to cache
+        if (Settings::isCacheEnabled()) {
+            Cacher::getInstance()->getDefaultCacher()->set($cache_key, $res);
+        }
+
+
+        return $res;
+    }
+
+    private static function getControllerCustomComponents($class)
+    {
+        $controller_name = ucfirst($class) . 'Controller';
+        /** @var Controller $controller_name */
+
+        $custom_components = [];
+        foreach ($controller_name::getComponents() as $key => $component) {
+            if (!isset($component['type']) || $component['type'] != 'custom') {
+                continue;
+            }
+
+            $custom_components[$class . '_' . $key] = $component['fields'];
+        }
+
+        return $custom_components;
+    }
+}
