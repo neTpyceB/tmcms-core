24d3c5aad1323bf5f4ee7cbf8878e2e3384bf65f
neTpyceB <pvman@inbox.ru>
2017-02-02 14:06:37 +0200

    Widgt class for setting customizer for input fields

---
+++ src/HTML/Widget.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace TMCms\HTML;
+
+defined('INC') or exit;
+
+/**
+ * Class Widget
+ */
+abstract class Widget
+{
+    /**
+     * @var Element
+     */
+    protected $owner;
+    protected $popup_url;
+
+    /**
+     * @param Element $owner
+     */
+    public function __construct(Element $owner = null)
+    {
+        $this->owner = $owner;
+    }
+
+    /**
+     * @param string $url
+     * @return $this
+     */
+    public function setModalPopupAjaxUrl($url) {
+        $this->popup_url = $url;
+
+        return $this;
+    }
+
+    /**
+     * @param Element $owner
+     * @return $this
+     */
+    public function setOwner(Element $owner)
+    {
+        $this->owner = $owner;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    abstract public function __toString();
+}
