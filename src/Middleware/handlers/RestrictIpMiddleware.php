7fc0fcc6b007232bc6145f5eb4de236912d582e5
neTpyceB <pvman@inbox.ru>
2017-04-17 22:42:29 +0300

    IP restriction moved to middleware

---
+++ src/Middleware/handlers/RestrictIpMiddleware.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Updated by neTpyceB [devp.eu] at 2017.4.17
+ */
+
+declare(strict_types=1);
+
+use TMCms\Config\Settings;
+use TMCms\Log\FrontendLogger;
+use TMCms\Routing\Interfaces\IMiddleware;
+
+class RestrictIpMiddleware implements IMiddleware
+{
+    public function run(array $params = [])
+    {
+        if (!isset($params['ips'])) {
+            return;
+        }
+
+        // IPs are separated by newline
+        $ips = explode("\n", $params['ips']);
+        // Remove empty lines and odd spaces
+        foreach ($ips as & $v) {
+            $v = trim($v);
+        }
+
+        // If still have IP in range and client is blocked - show error
+        if ($ips && !in_array(IP, $ips)) {
+
+            if (Settings::isFrontendLogEnabled()) {
+                FrontendLogger::getInstance()->err('IP ' . IP . ' forbidden');
+            }
+
+            if (!headers_sent()) {
+                header('HTTP/1.1 403 Forbidden');
+            }
+            die('Error 403. Forbidden');
+        }
+    }
+}
