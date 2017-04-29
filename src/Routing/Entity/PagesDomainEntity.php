21c41ed77e9109ede969a1e95f1d63a41052923a
neTpyceB <pvman@inbox.ru>
2017-04-29 23:59:02 +0300

    Domain entity doc block added

--- src/Routing/Entity/PagesDomainEntity.php
+++ src/Routing/Entity/PagesDomainEntity.php
@@ -4,6 +4,17 @@ namespace TMCms\Routing\Entity;
 
 use TMCms\Orm\Entity;
 
+/**
+ * Class PagesDomainEntity
+ * @package TMCms\Routing\Entity
+ *
+ * @method array getLanguages()
+ * @method string getName()
+ * @method string getUrls()
+ *
+ * @method $this setLanguages(array $languages)
+ * @method $this setUrls(array $urls)
+ */
 class PagesDomainEntity extends Entity
 {
 
