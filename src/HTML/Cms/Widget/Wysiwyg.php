55ee112f7c1edb126c1fae397befe3784b52b492
Vadims Petrusevs <pvman@inbox.ru>
2018-03-18 21:30:43 +0100

    Constants for easy access of string names

--- src/HTML/Cms/Widget/Wysiwyg.php
+++ src/HTML/Cms/Widget/Wysiwyg.php
@@ -7,52 +7,50 @@ use TMCms\HTML\Element;
 use TMCms\HTML\Widget;
 use TMCms\Templates\PageHead;
 
-defined('INC') or exit;
+\defined('INC') or exit;
 
+/**
+ * Class Wysiwyg
+ * @package TMCms\HTML\Cms\Widget
+ */
 class Wysiwyg extends Widget
 {
-	public $wysiwyg_options = '';
+    const FIELD_EDITOR_NAME = 'wysiwyg';
 
-	/**
-	 * @param Element $owner
-	 */
-	public function __construct(Element $owner = null)
-	{
-		parent::__construct($owner);
-	}
+    public $wysiwyg_options = '';
 
-	/**
-	 *
-	 * @param Element $owner
-	 * @return Wysiwyg
-	 */
-	public static function getInstance(Element $owner = null)
-	{
-		return new self($owner);
-	}
+    /**
+     *
+     * @param Element $owner
+     * @return $this
+     */
+    public static function getInstance(Element $owner = null): self
+    {
+        return new self($owner);
+    }
 
-	/**
-	 * @return string
-	 */
-	public function __toString()
-	{
-		PageHead::getInstance()->addJsUrl('/vendor/devp-eu/tmcms-core/src/assets/tinymce/tinymce.js');
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        PageHead::getInstance()->addJsUrl('/vendor/devp-eu/tmcms-core/src/assets/tinymce/tinymce.js');
 
-		ob_start();
+        ob_start();
 
-		?>
-		<span class="input-group-btn">
-			<button
+        ?>
+        <span class="input-group-btn">
+            <button
                     class="btn btn-success"
                     type="button"
                     data-popup-url="?p=components&do=wysiwyg&selector=<?= $this->owner->getId() ?>&options=<?= $this->wysiwyg_options ?>&nomenu&cache=<?= NOW ?>"
                     data-popup-result-destination="#<?= $this->owner->getId() ?>"
-				><i class="fa fa-arrow-left fa-fw"></i>
-				<?= __('Editor') ?>
-			</button>
-		</span>
-		<?php
+                ><i class="fa fa-arrow-left fa-fw"></i>
+                <?= __('Editor') ?>
+            </button>
+        </span>
+        <?php
 
-		return ob_get_clean();
-	}
+        return ob_get_clean();
+    }
 }
