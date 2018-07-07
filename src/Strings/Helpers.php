b7c16d2e877ae6f25414626d421ae985803b86c5
Vadims Petrusevs <pvman@inbox.ru>
2018-07-07 22:38:45 +0200

    Paginator example updated to show dots (skipped pages)

--- src/Strings/Helpers.php
+++ src/Strings/Helpers.php
@@ -14,7 +14,8 @@ defined('INC') or exit;
 class Helpers
 {
     /**
-     * Pagination, copy and modify where you need it
+     * Pagination, copy and modify where you need it - do not use this method originally as it may change, use only copied
+     *
      * @param int $page
      * @param int $per_page
      * @param int $total_products
@@ -32,19 +33,52 @@ class Helpers
 
         // Found from - till
         $start_from = $page * $per_page + 1;
-        $last_product_number = $start_from + $total_found_products_with_filters - 2;
+        $last_product_number = $start_from + $per_page - 1;
         $reached_end = false;
         if ($last_product_number >= $total_found_products_with_filters) {
             $last_product_number = $total_found_products_with_filters;
             $reached_end = true;
         }
+
+        $items_around_current = 3;
+        $previous_draw_dots = false;
+
+        \ob_start();
+
         ?>
         <div class="pagination">
             <?php if ($page > 0): ?>
                 <a class="pagination__prev" href="?<?= $url ?>&page=<?= $page ?>"></a>
             <?php endif; ?>
-            <?php for ($i = 1; $i <= $pages; $i++): ?>
-                <a class="pagination__link<?= $page === ($i - 1) ? ' active' : '' ?>" href="?<?= $url ?>&page=<?= $i ?>"><?= $i ?></a>
+            <?php for ($i = 1; $i <= $pages; $i++):
+                $should_draw_dots = true;
+
+                // Before and after selected number
+                if (abs($page - $i + 1) < $items_around_current) {
+                    $should_draw_dots = false;
+                }
+
+                // But not first items
+                if ($i + 1 - $items_around_current < $items_around_current) {
+                    $should_draw_dots = false;
+                }
+
+                // But not last items
+                if ($pages - $i - 1 < $items_around_current) {
+                    $should_draw_dots = false;
+                }
+
+                if ($should_draw_dots):
+                    // If previous render was dots - skip
+                    if ($previous_draw_dots) {
+                        continue; //
+                    }
+                    $previous_draw_dots = true; ?>
+                    <a class="pagination__link">...</a>
+                <?php else:
+                    $previous_draw_dots = false; ?>
+                    <a class="pagination__link<?= $page === ($i - 1) ? ' active' : '' ?>" href="?<?= $url ?>&page=<?= $i ?>"><?= $i ?></a>
+                <?php endif; ?>
             <?php endfor; ?>
             <?php if (!$reached_end): ?>
                 <a class="pagination__next" href="?<?= $url ?>&page=<?= $page + 2 ?>"></a>
@@ -53,6 +87,6 @@ class Helpers
         <?= $start_from ?> - <?= $last_product_number ?> <?= w('pager_table_from') ?> <?= $total_products ?>
         <?php
 
-        return ob_get_clean();
+        return \ob_get_clean();
     }
 }
